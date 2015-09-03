c0faa5b71ac2036f7694d7b2e12e099473bf4e85
ptjoker95 <ptjoker95@gmail.com>
2015-09-03 17:09:22 +0900

    일부 파일 번역. 반복적인 작업을 하는 after-translation.sh 수정

--- after_translation.sh
+++ after_translation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sed -i "s/Example/예제/g" $1
-sed -i "s/Prev/이전/g" $1
-sed -i "s/Next/다음/g" $1
-sed -i "s/Home/처음/g" $1
-sed -i "s/Up/위/g" $1
+DOCDIR=./*
+
+for file in $DOCDIR
+do
+  sed -i "s/예제/예제/g" $file
+  sed -i "s/이전/이전/g" $file
+  sed -i "s/다음/다음/g" $file
+  sed -i "s/처음/처음/g" $file
+  sed -i "s/위/위/g" $file
+done
+
+
